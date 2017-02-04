0dcb31c21d1230c4e664dfc3a935f137ac454345
Ericlin <nameljh1001@gmail.com>
2017-02-04 15:39:38 +0800

    push by script for backup

--- createRemoteRepo.sh
+++ createRemoteRepo.sh
@@ -1,19 +1,9 @@
-
-
-####Github-create()###################
 github-create() {
 	repo_name=$1
 	dir_name=`basename $(pwd)`
-
-	if [ "$repo_name" = "" ]; then
-		echo "Repo name (hit enter to use '$dir_name')?"
-		#read repo_name
-	fi
-
 	if [ "$repo_name" = "" ]; then
 		repo_name=$dir_name
 	fi
-
 	invalid_credentials=0
 	username=`git config github.user`
 	if [ "$username" = "" ]; then
@@ -21,7 +11,6 @@ github-create() {
 		invalid_credentials=1
 	fi
 	pass=`git config github.pass`
-	echo "using user:pass as  '$username':'$pass'"
 	if [ "$pass" = "" ]; then
 		echo "Could not find pass, run 'git config --global github.pass <pass>'"
 		invalid_credentials=1
